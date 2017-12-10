bb01b9319b8ba78366005c9a47e8bba97e787b44
RaviVats896 <vats.ravi96@gmail.com>
2017-12-10 22:40:11 +0530

    added top-n recommendation function.

--- movie-recommender.py
+++ movie-recommender.py
@@ -1,9 +1,37 @@
-import zipfile
-import time
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+import zipfile, time
 from surprise import Reader,  Dataset, evaluate, print_perf
 from surprise import SVD, SVDpp, NMF, KNNWithMeans, SlopeOne, CoClustering
+from collections import  defaultdict
 
+def get_top_n(predictions, n=10):
+    '''Return the top-N recommendation for each user from a set of predictions.
 
+    Args:
+        predictions(list of Prediction objects): The list of predictions, as
+            returned by the test method of an algorithm.
+        n(int): The number of recommendation to output for each user. Default
+            is 10.
+
+    Returns:
+    A dict where keys are user (raw) ids and values are lists of tuples:
+        [(raw item id, rating estimation), ...] of size n.
+    '''
+
+    # Map the predictions to each user.
+    top_n = defaultdict(list)
+    for uid, iid, true_r, est, _ in predictions:
+        top_n[uid].append((iid, est))
+
+    # Sort the predictions for each user and retrieve the k highest ones.
+    for uid, user_ratings in top_n.items():
+        user_ratings.sort(key=lambda x: x[1], reverse=True)
+        top_n[uid] = user_ratings[:n]
+
+    return top_n
+	
+	
 # Unzip ml-100k.zip
 zipfile = zipfile.ZipFile('ml-100k.zip', 'r')
 zipfile.extractall()
@@ -50,21 +78,32 @@ while(cont):
 	# Record algorithm execution end time and calculate execution time
 	print("Execution time: --- %s seconds ---" % (time.time() - start_time))
 	
+	# Retrieve the training set.
+	trainset = data.build_full_trainset()
+	algo.train(trainset)
+	
 	print("Choose an option:\n")
-	print("1. Predict rating of a certain item for a certain user.\n2. Skip this option.\n")
+	print("1. Predict rating of a certain item for a certain user.\n2. Get top n movie recommendations for a certain user.  \n3.Skip this option.\n")
 	choice= int(input())
 	if (choice == 1):
 		userid = str(input("Enter the User ID: "))
 		itemid = str(input("Enter the Item ID: "))
-		# Retrieve the trainset.
-		trainset = data.build_full_trainset()
-		algo.train(trainset)
-
+		
 		# Predict a certain item, userid = str(196), itemid = str(302)
 		actual_rating = 4
-		print(algo.predict(userid, itemid, actual_rating))		
-	
+		print(algo.predict(userid, itemid, actual_rating))	
+		
+	elif (choice  == 2):
+		print("\nEnter the User IDs (seperated by spaces):\n")
+		userlist = map(str, input().split())
+		testset = trainset.build_anti_testset()
+		predictions = algo.test(testset)
+		
+		top_n = get_top_n(predictions, n=10)
+		# Print the recommended items for each user
+		for uid in userlist:
+			print(uid, [iid for (iid, _) in top_n.get(uid)])
+		
 	print("Press 1 to continue, 0 to quit.\n")
 	cont= int(input())
-
-
+	
